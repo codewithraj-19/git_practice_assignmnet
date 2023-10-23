0750e69e9fce30b31bd5470d0c11f2519873cce1
SHiiKARii <rituraj282@gmail.com>
2023-10-23 16:12:50 +0530

    prime_and_palindrome_code

---
+++ prime.js
@@ -0,0 +1,17 @@
+let num = 7;
+let count = 0;
+
+for(let i=1; i<=num; i++)
+{
+    if(num%i == 0)
+    {
+      count++
+    }
+}
+if(count==1)
+{
+    console.log(num,"is prime");
+}
+else{
+    console.log(num,"is not prime");
+}
